e5b50b8bd130d06885d037737f68a4db46a78f52
Jardeylson Jacinto <jardeylsong.m@gmail.com>
2025-03-06 10:49:10 -0300

    Project CoreTrack created

---
+++ src/test/java/com/acc/web_service/WebServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.acc.web_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
