8e00f1c7f125a70010cbb43840f9eb88ef12e9d4
Jardeylson Jacinto <jardeylsong.m@gmail.com>
2025-03-07 16:52:54 -0300

    feat: H2 database, test profile, JPA

--- src/main/java/com/acc/web_service/model/User.java
+++ src/main/java/com/acc/web_service/model/User.java
@@ -2,6 +2,11 @@ package com.acc.web_service.model;
 
 import java.io.Serializable;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -9,9 +14,13 @@ import lombok.NoArgsConstructor;
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
+@Entity
+@Table(name = "tb_user")
 public class User implements Serializable {
   private static final long serialVersionUID = 1L;
 
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
   private Long id;
   private String name;
   private String email;
