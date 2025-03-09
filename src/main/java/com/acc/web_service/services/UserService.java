20c926991d5fedc3c3bc5ff57597e73fbe49f5db
Jardeylson Jacinto <jardeylsong.m@gmail.com>
2025-03-09 14:10:08 -0300

    feat: created userServicer

---
+++ src/main/java/com/acc/web_service/services/UserService.java
@@ -0,0 +1,26 @@
+package com.acc.web_service.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.acc.web_service.model.User;
+import com.acc.web_service.repository.UserRepository;
+
+@Service
+public class UserService {
+
+  @Autowired
+  private UserRepository repository;
+
+  public List<User> findAll() {
+    return repository.findAll();
+  }
+
+  public User findById(Long id) {
+    Optional<User> obj = repository.findById(id);
+    return obj.get();
+  }
+}
