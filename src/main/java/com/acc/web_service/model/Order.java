9f08b7c7d24be544605c829bd78ab77ecbfabf38
Jardeylson Jacinto <jardeylsong.m@gmail.com>
2025-03-09 14:37:53 -0300

    feat: Order, Instant, ISO 8601 - PART 1

--- src/main/java/com/acc/web_service/model/Order.java
+++ src/main/java/com/acc/web_service/model/Order.java
@@ -1,14 +1,14 @@
 package com.acc.web_service.model;
 
 import java.io.Serializable;
-import java.util.ArrayList;
-import java.util.List;
+import java.time.Instant;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import jakarta.persistence.OneToMany;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -18,22 +18,16 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @NoArgsConstructor
 @Entity
-@Table(name = "tb_user")
-public class User implements Serializable {
+@Table(name = "tb_order")
+public class Order implements Serializable {
   private static final long serialVersionUID = 1L;
 
   @Id
   @GeneratedValue(strategy = GenerationType.IDENTITY)
   private Long id;
-  private String name;
-  private String email;
-  private String phone;
-  private String password;
+  private Instant moment;
 
-  @OneToMany(mappedBy = "client")
-  private List<Order> orders = new ArrayList<>();
-
-  public List<Order> getOrders() {
-    return orders;
-  }
+  @ManyToOne
+  @JoinColumn(name = "client_id")
+  private User client;
 }
