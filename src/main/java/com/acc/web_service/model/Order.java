3b511e30aeeafd52d8ea5812af09e52c435d0de6
Jardeylson Jacinto <jardeylsong.m@gmail.com>
2025-03-10 09:33:30 -0300

    feat: added OrderStatus enum

--- src/main/java/com/acc/web_service/model/Order.java
+++ src/main/java/com/acc/web_service/model/Order.java
@@ -3,6 +3,8 @@ package com.acc.web_service.model;
 import java.io.Serializable;
 import java.time.Instant;
 
+import com.acc.web_service.model.enums.OrderStatus;
+
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -26,8 +28,27 @@ public class Order implements Serializable {
   @GeneratedValue(strategy = GenerationType.IDENTITY)
   private Long id;
   private Instant moment;
+  private Integer orderStatus;
 
   @ManyToOne
   @JoinColumn(name = "client_id")
   private User client;
+
+  public Order(Long id, Instant moment, OrderStatus orderStatus, User client) {
+    super();
+    this.id = id;
+    this.moment = moment;
+    this.client = client;
+    setOrderStatus(orderStatus);
+  }
+
+  public OrderStatus getOrderStatus() {
+    return OrderStatus.valueOf(orderStatus);
+  }
+
+  public void setOrderStatus(OrderStatus orderStatus) {
+    if (orderStatus != null) {
+      this.orderStatus = orderStatus.getCode();
+    }
+  }
 }
