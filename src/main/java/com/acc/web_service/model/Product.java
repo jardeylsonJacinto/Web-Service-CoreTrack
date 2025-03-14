9161891b5c344c36ae84840dd520d2666c44dcd2
Jardeylson Jacinto <jardeylsong.m@gmail.com>
2025-03-14 10:46:59 -0300

    refactor: Product-OrderItem one-to-many association

--- src/main/java/com/acc/web_service/model/Product.java
+++ src/main/java/com/acc/web_service/model/Product.java
@@ -4,6 +4,8 @@ import java.io.Serializable;
 import java.util.HashSet;
 import java.util.Set;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -11,6 +13,7 @@ import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.JoinTable;
 import jakarta.persistence.ManyToMany;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -36,6 +39,9 @@ public class Product implements Serializable {
   @JoinTable(name = "tb_product_category", joinColumns = @JoinColumn(name = "product_id"), inverseJoinColumns = @JoinColumn(name = "category_id"))
   private Set<Category> categories = new HashSet<>();
 
+  @OneToMany(mappedBy = "id.product")
+  private Set<OrderItem> items = new HashSet<>();
+
   public Product(Long id, String name, String description, Double price, String imgUrl) {
     super();
     this.id = id;
@@ -43,5 +49,14 @@ public class Product implements Serializable {
     this.description = description;
     this.price = price;
     this.imgUrl = imgUrl;
+  }
+
+  @JsonIgnore
+  public Set<Order> getOrders() {
+    Set<Order> set = new HashSet<>();
+    for (OrderItem x : items) {
+      set.add(x.getOrder());
+    }
+    return set;
   }
 }
