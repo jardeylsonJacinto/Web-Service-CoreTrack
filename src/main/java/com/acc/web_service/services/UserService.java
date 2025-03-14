95e27ab538da94ab5b644ae7758a8bbe7f543671
Jardeylson Jacinto <jardeylsong.m@gmail.com>
2025-03-14 11:41:03 -0300

    feat: Exception handling - update

--- src/main/java/com/acc/web_service/services/UserService.java
+++ src/main/java/com/acc/web_service/services/UserService.java
@@ -13,6 +13,8 @@ import com.acc.web_service.exceptions.ResourceNotFoundException;
 import com.acc.web_service.model.User;
 import com.acc.web_service.repository.UserRepository;
 
+import jakarta.persistence.EntityNotFoundException;
+
 @Service
 public class UserService {
 
@@ -43,9 +45,13 @@ public class UserService {
   }
 
   public User update(Long id, User obj) {
-    User entity = repository.getReferenceById(id);
-    updateData(entity, obj);
-    return repository.save(entity);
+    try {
+      User entity = repository.getReferenceById(id);
+      updateData(entity, obj);
+      return repository.save(entity);
+    } catch (EntityNotFoundException e) {
+      throw new ResourceNotFoundException(id);
+    }
   }
 
   private void updateData(User entity, User obj) {
