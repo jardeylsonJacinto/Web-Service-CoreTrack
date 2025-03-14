393718e2953613bfa704bd579c803abdde562c82
Jardeylson Jacinto <jardeylsong.m@gmail.com>
2025-03-14 10:52:41 -0300

    feat: Payment, one-to-one association

--- src/main/java/com/acc/web_service/config/TestConfig.java
+++ src/main/java/com/acc/web_service/config/TestConfig.java
@@ -11,6 +11,7 @@ import org.springframework.context.annotation.Profile;
 import com.acc.web_service.model.Category;
 import com.acc.web_service.model.Order;
 import com.acc.web_service.model.OrderItem;
+import com.acc.web_service.model.Payment;
 import com.acc.web_service.model.Product;
 import com.acc.web_service.model.User;
 import com.acc.web_service.model.enums.OrderStatus;
@@ -80,6 +81,11 @@ public class TestConfig implements CommandLineRunner {
     OrderItem oi4 = new OrderItem(o3, p5, 2, p5.getPrice());
 
     orderItemRepository.saveAll(Arrays.asList(oi1, oi2, oi3, oi4));
+
+    Payment pay1 = new Payment(null, Instant.parse("2019-06-20T21:53:07Z"), o1);
+    o1.setPayment(pay1);
+
+    orderRepository.save(o1);
 
   }
 }
